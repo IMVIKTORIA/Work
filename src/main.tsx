0a2100e18e135fb7adc2c47e0dd87e7e5169ee68
Назаров Антон <nazarov@elewise.local>
2024-10-18 10:08:17 +0300

    feat: Переименование элемента root

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App/App'
 
 // Рендер приложений
-ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
+ReactDOM.createRoot(document.querySelector('#approval-tab-root') as HTMLElement).render(
 	<App />
 )
