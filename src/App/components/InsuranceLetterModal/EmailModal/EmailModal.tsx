d6850b186b7b8dbe113b81a03262b643573ab54f
Виктория <imviiktoria@MacBook-Air-Viktoria.local>
2024-12-10 13:40:43 +0400

    feat:<изменения>

--- src/App/components/InsuranceLetterModal/EmailModal/EmailModal.tsx
+++ src/App/components/InsuranceLetterModal/EmailModal/EmailModal.tsx
@@ -1,63 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import Scripts from '../../../shared/utils/clientScripts';
-import Loader from '../../Loader/Loader';
-import { ButtonType } from '../../../shared/types';
-import Button from '../../Button/Button';
+import React, { useEffect, useState } from "react";
+import Scripts from "../../../shared/utils/clientScripts";
+import Loader from "../../Loader/Loader";
+import { ButtonType } from "../../../shared/types";
+import Button from "../../Button/Button";
 
 interface EmailModalProps {
-	/** Сохранить проект */
-	handleSaveClick: (text: string) => void
-	/** Отмена */
-	handleCancelClick: () => void
-	/** Идентификатор согласования */
-	approvalId: string
+  /** Сохранить проект */
+  handleSaveClick: (text: string) => void;
+  /** Отмена */
+  handleCancelClick: () => void;
+  /** Идентификатор согласования */
+  approvalId: string;
 }
 
 /** Модальное окно гарантийного письма (Email) */
-export default function EmailModal({ handleSaveClick, handleCancelClick, approvalId }: EmailModalProps) {
-	// Флаг загрузки текста по шаблону
-	const [isTextLoading, setIsTextLoading] = useState<boolean>(false);
-	// Текст
-	const [text, setText] = useState<string>("");
+export default function EmailModal({
+  handleSaveClick,
+  handleCancelClick,
+  approvalId,
+}: EmailModalProps) {
+  // Флаг загрузки текста по шаблону
+  const [isTextLoading, setIsTextLoading] = useState<boolean>(false);
+  // Текст
+  const [text, setText] = useState<string>("");
 
-	// Генерация текста по шаблону
-	useEffect(() => {
-		setIsTextLoading(true);
-		Scripts.generateEmailText().then(text => {
-			setText(text)
-			setIsTextLoading(false);
-		})
-	}, [])
+  // Генерация текста по шаблону
+  useEffect(() => {
+    setIsTextLoading(true);
+    Scripts.generateEmailText().then((text) => {
+      setText(text);
+      setIsTextLoading(false);
+    });
+  }, []);
 
-	const onClickSave = async () => {
-		await Scripts.savePaperApproval(approvalId, text);
-		handleSaveClick(text)
-	}
+  const onClickSave = async () => {
+    await Scripts.saveEmailApproval(approvalId, text);
+    handleSaveClick(text);
+  };
 
-	const onClickCancel = async () => {
-		handleCancelClick()
-	}
+  const onClickCancel = async () => {
+    handleCancelClick();
+  };
 
-	return (
-		<div className='insurance-letter-modal'>
-			<div className="insurance-letter-modal__header">
-				<span className="insurance-letter-modal__label">Гарантийное письмо email</span>
-			</div>
-			<div className='insurance-letter-modal__content' style={{ width: "500px" }}>
-				{/* Текст */}
-				<div className='insurance-letter-modal__text'>
-					{
-						isTextLoading
-							? <Loader />
-							: text
-					}
-				</div>
-				{/* Кнопки */}
-				<div className='insurance-letter-modal__buttons'>
-					{!isTextLoading && <Button title={"Подтвердить и сохранить"} clickHandler={onClickSave} />}
-					<Button title={"Отмена"} buttonType={ButtonType.outline} clickHandler={onClickCancel} />
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="insurance-letter-modal">
+      <div className="insurance-letter-modal__header">
+        <span className="insurance-letter-modal__label">
+          Гарантийное письмо email
+        </span>
+      </div>
+      <div
+        className="insurance-letter-modal__content"
+        style={{ width: "500px" }}
+      >
+        {/* Текст */}
+        <div className="insurance-letter-modal__text">
+          {isTextLoading ? <Loader /> : text}
+        </div>
+        {/* Кнопки */}
+        <div className="insurance-letter-modal__buttons">
+          {!isTextLoading && (
+            <Button
+              title={"Подтвердить и сохранить"}
+              clickHandler={onClickSave}
+            />
+          )}
+          <Button
+            title={"Отмена"}
+            buttonType={ButtonType.outline}
+            clickHandler={onClickCancel}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
